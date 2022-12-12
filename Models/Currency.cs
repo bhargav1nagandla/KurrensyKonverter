b0a43b25c65a6c1d43878dcb281856577f81dbf5
Mahiwsu <115042055+Mahiwsu@users.noreply.github.com>
2022-12-11 22:37:32 -0600

    Structure for ConversionFunction

---
+++ Models/Currency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Models
+{
+    public class Currency
+    {
+        public float amount { get; set; }
+
+        [Column("base")]
+        public string _base { get; set; }
+
+        public string _date { get; set; }
+
+        public string rates {get;set;}
+
+     
+
+
+    }
+}
